1f3bf691e3c7e06c295cd5c1edcb00b298963b1a
ikolesoo <eak-2004@mail.ru>
2015-09-23 11:18:19 +0300

    Test Commit

--- Calc/Cacl.cpp
+++ Calc/Cacl.cpp
@@ -1,1 +1,8 @@
 #include <cstdio>
+#include "Calc.h"
+#include <iostream>
+
+Calc::Calc()
+{
+	std::cout << "Calc created\n";
+}
--- Calc/Calc.h
+++ Calc/Calc.h
@@ -1,9 +1,16 @@
+#include <iostream>
 class Calc {
 	public:
 		Calc();
 		void Add(int a, int b);
 		void Sub(int a, int b);
-
-
-
+		void Mult(int a, int b);
+		void div(int a, int b);
+		virtual void print() const;
+		virtual ~Calc();
+		
 };
+Calc::Calc()
+{
+	std::cout << "Calc created\n";
+}
